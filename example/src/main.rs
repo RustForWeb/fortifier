59f2038a097a1ef23ea8fbfa49c22cb0999429ef
Daniëlle Huisman <danielle@huisman.me>
2025-12-06 11:16:06 +0100

    feat: add custom validation (#8)

--- example/src/main.rs
+++ example/src/main.rs
@@ -12,16 +12,32 @@ struct CreateUser {
 
     #[validate(url)]
     url: String,
+
+    #[validate(custom(function = validate_one_locale_required, error = OneLocaleRequiredError))]
+    locales: Vec<String>,
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+#[derive(Debug)]
+struct OneLocaleRequiredError;
+
+fn validate_one_locale_required(locales: &[String]) -> Result<(), OneLocaleRequiredError> {
+    if locales.is_empty() {
+        Err(OneLocaleRequiredError)
+    } else {
+        Ok(())
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     let data = CreateUser {
         email: "john@doe.com".to_owned(),
         name: "John Doe".to_owned(),
         url: "https://john.doe.com".to_owned(),
+        locales: vec!["en_GB".to_owned()],
     };
 
-    data.validate_sync()?;
+    data.validate().await?;
 
     Ok(())
 }
