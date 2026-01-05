93f267363816c21fe5c85b44ba13e4fd23e56276
Daniëlle Huisman <danielle@huisman.me>
2026-01-05 13:49:39 +0100

    fix: use `cfg-if` for conditional feature in unit test (#68)

--- packages/fortifier/tests/serde.rs
+++ packages/fortifier/tests/serde.rs
@@ -1,5 +1,6 @@
 #![cfg(feature = "serde")]
 
+use cfg_if::cfg_if;
 use fortifier::{
     EmailAddressError, LengthError, LengthErrorCode, RegexError, UrlError, ValidationErrors,
 };
@@ -18,66 +19,74 @@ enum TestError {
 }
 
 fn setup() -> (ValidationErrors<TestError>, Value) {
-    (
-        ValidationErrors::from_iter([
-            TestError::EmailAddress(EmailAddressError::from(
-                email_address::Error::MissingSeparator,
-            )),
-            TestError::Length(LengthError::Equal {
-                equal: 1,
-                value: 2,
-                code: LengthErrorCode,
-                #[cfg(feature = "message")]
-                message: "length 2 is not equal to required length 1".to_owned(),
-            }),
-            TestError::Regex(RegexError::default()),
-            TestError::Url(UrlError::from(ParseError::EmptyHost)),
-        ]),
-        #[cfg(not(feature = "message"))]
-        json!([
-            {
-                "code": "emailAddress",
-                "subcode": "missingSeparator",
-            },
-            {
-                "code": "length",
-                "subcode": "equal",
-                "equal": 1,
-                "value": 2,
-            },
-            {
-                "code": "regex",
-            },
-            {
-                "code": "url",
-                "subcode": "emptyHost",
-            }
-        ]),
-        #[cfg(feature = "message")]
-        json!([
-            {
-                "code": "emailAddress",
-                "subcode": "missingSeparator",
-                "message": "",
-            },
-            {
-                "code": "length",
-                "subcode": "equal",
-                "equal": 1,
-                "value": 2,
-                "message": "length 2 is not equal to required length 1",
-            },
-            {
-                "code": "regex",
-                "message": "value does not match regular expression",
-            },
-            {
-                "code": "url",
-                "subcode": "emptyHost",
-                "message": "empty host",
-            }
-        ]),
-    )
+    let errors = ValidationErrors::from_iter([
+        TestError::EmailAddress(EmailAddressError::from(
+            email_address::Error::MissingSeparator,
+        )),
+        TestError::Length(LengthError::Equal {
+            equal: 1,
+            value: 2,
+            code: LengthErrorCode,
+            #[cfg(feature = "message")]
+            message: "length 2 is not equal to required length 1".to_owned(),
+        }),
+        TestError::Regex(RegexError::default()),
+        TestError::Url(UrlError::from(ParseError::EmptyHost)),
+    ]);
+
+    cfg_if! {
+        if #[cfg(feature = "message")] {
+             (
+                errors,
+                json!([
+                    {
+                        "code": "emailAddress",
+                        "subcode": "missingSeparator",
+                        "message": "",
+                    },
+                    {
+                        "code": "length",
+                        "subcode": "equal",
+                        "equal": 1,
+                        "value": 2,
+                        "message": "length 2 is not equal to required length 1",
+                    },
+                    {
+                        "code": "regex",
+                        "message": "value does not match regular expression",
+                    },
+                    {
+                        "code": "url",
+                        "subcode": "emptyHost",
+                        "message": "empty host",
+                    }
+                ]),
+            )
+        } else {
+            (
+                errors,
+                json!([
+                    {
+                        "code": "emailAddress",
+                        "subcode": "missingSeparator",
+                    },
+                    {
+                        "code": "length",
+                        "subcode": "equal",
+                        "equal": 1,
+                        "value": 2,
+                    },
+                    {
+                        "code": "regex",
+                    },
+                    {
+                        "code": "url",
+                        "subcode": "emptyHost",
+                    }
+                ]),
+            )
+        }
+    }
 }
 
 #[test]
